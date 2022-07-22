23734cca68261ef249e2cbbc5381ba146630af71
PupperParty <93052348+PupperParty@users.noreply.github.com>
2022-07-21 17:15:36 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ draw_rect = "";
 draw_circle = "";
 
 var SpeechRecognition = window.webkitSpeechRecognition;
-var recognition = new SpeechRecognition();
+ var recognition = new SpeechRecognition();
 
 function start() {
     document.getElementById("status").innerHTML = "System is listening you may speak";
@@ -12,16 +12,16 @@ function start() {
 }
 
 recognition.onresult = function(event) {
-    console.log(event)
+    console.log(event);
     var content = event.results[0][0].transcript;
     document.getElementById("status").innerHTML = "System identified your fancy words to be: " + content;
-    if(content == "circle") {
-        x = Math.floor(Math.round() * 900);
-        y = Math.floor(Math.round() * 600);
+    if(content =="circle" || content =="Circle"){
+        x = Math.floor(Math.random() * 900);
+        y = Math.floor(Math.random() * 600);
         document.getElementById("status").innerHTML = "Your CirClE has started being drawn";
         draw_circle = "set";
     }
-    if(content == "rect") {
+    if(content =="rectangle" || content =="Rectangle") {
         x = Math.floor(Math.random() * 900);
         y = Math.floor(Math.random() * 600);
         document.getElementById("status").innerHTML = "Your ReCtAnGlE has started being drawn";
@@ -40,7 +40,7 @@ function draw() {
          document.getElementById("status").innerHTML = "Your CirCle had been drawn.";
          draw_circle = "";
     }
-    if(draw_circle == "set") {
+    if(draw_rect == "set") {
          rect(x, y, 70, 50);
          document.getElementById("status").innerHTML = "Your RecTanGLe had been drawn.";
          draw_rect = "";
